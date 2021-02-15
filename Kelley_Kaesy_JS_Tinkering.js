9aea9aa42ce5c1928b6f5710bcd2a0bef888f763
Kaesy Kelley <kaesykelley28@gmail.com>
2021-02-14 23:56:04 -0500

    Initial Commit

---
+++ Kelley_Kaesy_JS_Tinkering.js
@@ -0,0 +1,13 @@
+function displayIfChildIsAbleToRideTheRollerCoaster
+    var x = 54;
+    if (x > 52) {
+        console.log ("Get on that ride, kiddo!");
+    }
+    else {
+        console.log (Sorry kiddo. Maybe next year."")
+    }
+    
+    
+    
+    
+    }
