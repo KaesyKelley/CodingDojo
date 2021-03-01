f0b4384da92faf62963aaa426ed09f2bdebcd9f5
Kaesy Kelley <kaesykelley28@gmail.com>
2021-03-01 02:31:08 -0500

    Homework Assignments

---
+++ Javascript/playingwithobjects.js
@@ -0,0 +1,19 @@
+var user = {};
+user = {
+    name: ["Michael", "John", "David"],
+    age: [37, 30, 27],
+}
+console.log(user.name[0]);
+console.log(user.age[0]);
+// Michael's Name/Age
+
+var user = {};
+user = {
+    name: ["Michael", "John", "David"],
+    age: [37, 30, 27],
+}
+console.log(user.name[0]);
+// log name of first object
+
+
+
